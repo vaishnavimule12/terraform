2436f9cc7e45621d9645f826a6213f556b476d3e
vaishnavi mule <vaishnavimule128@gmail.com>
2026-06-10 08:47:02 +0530

    ec2 and variable files added

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "eu-north-1"
 }
+resource "aws_instance" "my_instace" {
+  ami             = "ami-05d62b9bc5a6ca605"
+  instance_type   = "t3.micro"
+  key_name        = "key_rsa"
+  security_groups = ["my-security-group"]
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "eu-north-1"
+}
+variable "ami" {
+  default = "ami-05d62b9bc5a6ca605"
+}
+variable "key_name" {
+  default = "key_rsa"
+}
+variable "security_group" {
+  default = "my-security-group"
+}
+variable "Name" {
+  default = "Terraform_instance"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "name" {
+  default = "instance"
+}
+variable "environment" {
+  default = "Devops"
+}
