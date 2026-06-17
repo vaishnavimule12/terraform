261e41d0e1bdc65b78a2c626c294cfd80704e985
vaishnavi mule <vaishnavimule128@gmail.com>
2026-06-17 09:10:00 +0530

    change region

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         =  "tfstate-vpc421"
+    bucket         = "tfstate-vpc421"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "value"
